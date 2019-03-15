b4e2286d16f4b64adcfcbf8a0c792fdcd7b4c85f
Vince Patrick Alferez <vintrickal@gmail.com>
2019-03-15 17:54:22 +0800

    Updates

---
+++ precache-manifest.896a0c6f2f3bbd4cbb0aa9d3d8f1d44f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19a8db08324e9b1cabe9",
+    "url": "/React-Calculator/static/js/runtime~main.cb778861.js"
+  },
+  {
+    "revision": "4187701a743bb18b6001",
+    "url": "/React-Calculator/static/js/main.11f08e49.chunk.js"
+  },
+  {
+    "revision": "fd7b5fc4ab407c4e0808",
+    "url": "/React-Calculator/static/js/2.e99ea143.chunk.js"
+  },
+  {
+    "revision": "4187701a743bb18b6001",
+    "url": "/React-Calculator/static/css/main.b73c1d46.chunk.css"
+  },
+  {
+    "revision": "1f1a1de62ce10bdafc95cef8df8f99fb",
+    "url": "/React-Calculator/index.html"
+  }
+];
